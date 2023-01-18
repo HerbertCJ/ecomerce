59c1efedda7179bfcb43453b352e4d0df574224f
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-18 09:48:38 -0300

    Updated filter reducer/context, added sort by method in Products

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,21 +15,49 @@ import { useProductsContext } from './products_context'
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
+  sort: 'price-lowest',
+  filters: {
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  }
 }
 
 const FilterContext = React.createContext()
 
 export const FilterProvider = ({ children }) => {
-  const {products} = useProductsContext()
+  const { products } = useProductsContext()
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    dispatch({type:LOAD_PRODUCTS, payload: products})
+    dispatch({ type: LOAD_PRODUCTS, payload: products })
   }, [products])
 
-  return (    
-    <FilterContext.Provider value={{...state}}>
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS })
+  }, [products, state.sort])
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW })
+  }
+
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
+
+  const updateSort = (e) => {
+    const value = e.target.value
+    dispatch({ type: UPDATE_SORT, payload: value })
+  }
+
+  return (
+    <FilterContext.Provider value={{ ...state, setGridView, setListView, updateSort }}>
       {children}
     </FilterContext.Provider>
   )
